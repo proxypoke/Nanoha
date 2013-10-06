4d85b75ae1c2f37d5c5a8bc9a9fb273bfe1a8625
slowpoke <mail+git@slowpoke.io>
2013-10-06 01:02:49 +0000

    Add internal call for enabling paging.

--- kernel/mm.c
+++ kernel/mm.c
@@ -30,7 +30,17 @@ void mm_free_pages(void* start, size_t n) {
     /* TODO */
 }
 
+static inline void _enable_paging(void) {
+    __asm__ volatile(
+             "movl %cr0, %eax\n"
+             "orl  $0x80000000, %eax\n"
+             "movl %eax, %cr0\n"
+    );
+}
+
 void mm_init(void) {
     first_free = (uintptr_t*) MEMORY_START;
     end_of_memory = first_free + get_memsize();
+
+    /*_enable_paging();*/
 }
